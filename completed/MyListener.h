930cde12aaa887ffa52b0752865cd71139a110ed
Jatus <gianmarco.pettinato@gmail.com>
2019-05-07 12:51:39 +0200

    Sistemata la struttura delle cartelle messo in risalto i file importanti

---
+++ completed/MyListener.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "antlr4-runtime.h"
+#include "tinyrexxParser.h"
+#include "tinyrexxBaseListener.h"
+
+/**
+ * This class defines a concrete listener for a parse tree produced by tinyrexxParser.
+ */
+class  MyListener : public tinyrexxBaseListener {
+private:
+  int indent = 0;
+  std::set<std::string> vars;
+
+public:
+  MyListener(const std::set<std::string> &ids);
+
+  void enterProgram(tinyrexxParser::ProgramContext * ctx);
+  void exitProgram(tinyrexxParser::ProgramContext * ctx);
+
+  void enterAssign(tinyrexxParser::AssignContext * ctx);
+  void exitAssign(tinyrexxParser::AssignContext * ctx);
+
+  void enterPrint(tinyrexxParser::PrintContext * ctx);
+  void exitPrint(tinyrexxParser::PrintContext * ctx);
+
+  void exitInput(tinyrexxParser::InputContext * ctx);
+
+  void enterW_loop(tinyrexxParser::W_loopContext * ctx);
+  void exitW_loop(tinyrexxParser::W_loopContext * ctx);
+
+  void enterF_loop(tinyrexxParser::F_loopContext * ctx);
+  void exitF_loop(tinyrexxParser::F_loopContext * ctx);
+
+  void enterL_proxy(tinyrexxParser::L_proxyContext * ctx);
+  void exitL_proxy(tinyrexxParser::L_proxyContext * ctx);
+
+  void enterA_expr(tinyrexxParser::A_exprContext * ctx);
+  void exitA_expr(tinyrexxParser::A_exprContext * ctx);
+
+  void exitA_op(tinyrexxParser::A_opContext * ctx);
+  void exitR_op(tinyrexxParser::R_opContext * ctx);
+
+  void enterTerminate(tinyrexxParser::TerminateContext * ctx);
+  void exitTerminate(tinyrexxParser::TerminateContext * ctx);
+
+  void enterI_op(tinyrexxParser::I_opContext* ctx);
+  void exitI_op(tinyrexxParser::I_opContext* ctx);
+
+  void enterBool_OP(tinyrexxParser::Bool_OPContext * ctx);
+  void exitBool_OP(tinyrexxParser::Bool_OPContext * ctx);
+
+  void exitL_else(tinyrexxParser::L_elseContext * ctx);
+
+  void exitB_op(tinyrexxParser::B_opContext* ctx);
+
+  void exitB_not(tinyrexxParser::B_notContext* ctx);
+
+  void exitTo_r(tinyrexxParser::To_rContext* ctx);
+};
---
+++ completed/tinyrexx.g4
@@ -0,0 +1,41 @@
+grammar tinyrexx;
+
+program   : statement+;
+
+statement : assign | print | input | w_loop | i_op | f_loop | terminate;
+
+assign    : ID '=' a_expr;
+print     : 'say' a_expr ;
+input     : 'pull' ID ;
+i_op      : 'if' l_proxy 'then' 'do' statement+ 'end' | 'if' l_proxy 'then' 'do' statement+ 'end' l_else 'do' statement+ 'end';
+w_loop    : 'do' 'while' l_proxy statement+ 'end' ;
+f_loop    : 'do' l_proxy statement+ 'end';
+l_else    : 'else';
+l_proxy   : bool_OP | ID '=' a_expr to_r a_expr;
+to_r      : 'to';
+a_expr    : ID | NUMBER | '(' a_expr ')' | a_expr a_op a_expr | MINUS a_expr ;
+a_op      : MINUS | PLUS | MUL | DIV ;
+r_op      : EQUAL | LT | LEQ | GT | GEQ ;
+terminate : 'exit' a_expr;
+bool_OP   : OPENP bool_OP CLOSEP | bool_OP b_op bool_OP | b_not bool_OP | a_expr r_op a_expr |a_expr;
+b_op      : AND | OR;
+b_not     : NOT ;
+
+OPENP     : '(' ;
+CLOSEP    : ')' ;
+MINUS     : '-' ;
+PLUS      : '+' ;
+MUL       : '*' ;
+DIV       : '/' ;
+EQUAL     : '==' ;
+LT        : '<' ;
+LEQ       : '<=' ;
+GT        : '>' ;
+GEQ       : '>=' ;
+AND       : '&' ;
+OR        : '|'  ;
+NOT       : '\\'   ;
+ID        : [a-z]+ ;
+NUMBER    : [0-9]+ ;
+WS        : [ \n\t]+ -> skip;
+ErrorChar : . ;
